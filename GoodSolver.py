0e738297fdd3b7e19208f52299e62f77c835e831
Bicskow <bicskow@gmail.com>
2017-10-23 10:19:30 +0200

    Fix for void

--- GoodSolver.py
+++ GoodSolver.py
@@ -6,6 +6,7 @@ class GoodSolver:
 
     def __init__(self):
         self.prefered_dir = 2
+        self.first_wall_found = False
 
     def move(self, state):
         #print("neigh:",state.neigh)
@@ -14,6 +15,12 @@ class GoodSolver:
         #sleep(1000000)
         #print("Preferred is" + str(self.prefered_dir))
         to_ret = 0
+
+        if not self.first_wall_found:
+            if state.neigh[self.prefered_dir] == 0:
+                return self.prefered_dir
+            else:
+                self.first_wall_found = True
 
         if state.neigh[(self.prefered_dir + 1) % 4] == 0:
             to_ret = (self.prefered_dir + 1) % 4
